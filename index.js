ac63b8b40137a33d7038d382ab27bd0df5a590fa
Michael Ballard <ballard.michael9@gmail.com>
2018-06-30 16:16:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(drivers){
-  return drivers.map(driver){
+  return drivers.map((driver)=>
     return driver.toLowerCase()
-  }
+    
+  })
+    
+  
 }
